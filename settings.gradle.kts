dcb9534615cdd327afa05e8c0e6cdb23b6978cb8
gonelayamuna <gonelayamuna@gmail.com>
2024-01-30 18:07:22 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "test_appium_poc"
+include(":app")
+ 
